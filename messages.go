cc51b231175d92cbc4706281e2f1b9decd6fceda
Oliver, Jonathan <git@jonathanoliver.com>
2016-11-08 10:58:20 -0700

    Stubbing out messages.

--- messages.go
+++ messages.go
@@ -1,28 +1,128 @@
 package acidic
 
-type LoadItemRequest struct{}
-type LoadItemResponse struct{}
+import (
+	"io"
+	"time"
+)
 
-type StoreItemCommand struct{}
-type TransactionStartedEvent struct{}
-type StoringItemEvent struct{}
-type ItemStoredEvent struct{}
-type ItemStoreFailedEvent struct{}
+type LoadItemRequest struct {
+	TransactionID string // optional
+	Key           string
+	ETag          string // optional
+	Context       *MessageContext
+}
+type LoadItemResponse struct {
+	TransactionID string // optional
+	ContentLength uint64
+	ContentType   string
+	Key           string
+	ETag          string
+	Metadata      map[string]string
+	Payload       io.Reader
+}
 
-type DeleteItemCommand struct{}
-type DeletingItemEvent struct{}
-type ItemDeletedEvent struct{}
-type ItemDeleteFailedEvent struct{}
+type StoreItemCommand struct {
+	Timestamp     time.Time
+	TransactionID string // optional (blank = new tx)
+	Key           string
+	ETag          string // optional
+	Metadata      map[string]string
+	Payload       io.Reader
+	Context       *MessageContext
+}
+type TransactionStartedEvent struct {
+	Timestamp     time.Time
+	TransactionID string
+	Started       time.Time
+	Expiration    time.Time
+}
+type StoringItemEvent struct {
+	Timestamp     time.Time
+	Sequence      uint64 // incremented for each mutating operation; this helps us to know which Store was the most recent one
+	TransactionID string
+	Key           string
+	ETag          string // optional
+	Payload       io.Reader
+	Metadata      map[string]string
+	Context       *MessageContext
+}
+type ItemStoredEvent struct {
+	Timestamp     time.Time
+	Sequence      uint64
+	TransactionID string
+	Key           string
+	Revision      string
+	ETag          string
+}
+type ItemStoreFailedEvent struct {
+	Timestamp     time.Time
+	Sequence      uint64
+	TransactionID string
+	Key           string
+}
 
-type CommitTransactionCommand struct{}
-type TransactionCommittingEvent struct{}
-type TransactionCommittedEvent struct{}
+type DeleteItemCommand struct {
+	Timestamp     time.Time
+	TransactionID string // optional (blank = new tx)
+	Key           string
+	ETag          string // optional
+	Context       *MessageContext
+}
+type DeletingItemEvent struct {
+	Timestamp     time.Time
+	Sequence      uint64
+	TransactionID string
+	Key           string
+	ETag          string // optional
+	Context       *MessageContext
+}
+type ItemDeletedEvent struct {
+	Timestamp     time.Time
+	Sequence      uint64
+	TransactionID string
+	Key           string
+}
+type ItemDeleteFailedEvent struct {
+	Timestamp     time.Time
+	Sequence      uint64
+	TransactionID string
+	Key           string
+}
 
-type TransactionFailedEvent struct{}
+type CommitTransactionCommand struct {
+	Timestamp     time.Time
+	TransactionID string
+	Context       *MessageContext
+}
+type TransactionCommittingEvent struct {
+	Timestamp     time.Time
+	TransactionID string
+	Context       *MessageContext
+}
+type TransactionCommittedEvent struct {
+	Timestamp     time.Time
+	TransactionID string
+}
 
-type AbortTransactionCommand struct{}
-type TransactionAbortedEvent struct{}
-type TransactionAbortFailedEvent struct{}
+type TransactionFailedEvent struct {
+	Timestamp     time.Time
+	TransactionID string
+	Reason        error
+}
+
+type AbortTransactionCommand struct {
+	Timestamp     time.Time
+	TransactionID string
+	Context       *MessageContext
+}
+type TransactionAbortedEvent struct {
+	Timestamp     time.Time
+	TransactionID string
+}
+type TransactionAbortFailedEvent struct {
+	Timestamp     time.Time
+	TransactionID string
+}
 
 type MessageContainer interface {
 	Add(interface{})
